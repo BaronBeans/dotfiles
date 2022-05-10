a60ef2c7603489f598bbd113ad6cb5168f9958ac
Sean Barnard <sean.barnard.94@hotmail.com>
2022-05-10 11:12:52 +0100

    Update nvim script with instructions on how to install from source

--- nvim/nvim.sh
+++ nvim/nvim.sh
@@ -1,5 +1,24 @@
 #!/bin/sh
 rm -rf ~/.config/nvim/lua
-cp -r ../lua ~/.config/nvim/lua
-cp ../init.lua ~/.config/nvim/init.lua
+cp -r ./lua ~/.config/nvim/lua
+cp ./init.lua ~/.config/nvim/init.lua
 
+# INSTALL NEOVIM:
+# ================
+# //https://github.com/neovim/neovim/wiki/Building-Neovim#build-prerequisites
+# //https://github.com/neovim/neovim/wiki/Installing-Neovim#install-from-source
+# sudo apt update -y
+# sudo apt install -y ninja-build gettext libtool libtool-bin autoconf automake cmake g++ pkg-config unzip
+# cd; mkdir clones; cd clones; git clone https://github.com/neovim/neovim.git; cd neovim
+# git tag
+# git checkout tags/desired_version_number -b tagName-branch
+# (or just use master for latest)
+# rm -rf build
+# make CMAKE_BUILD_TYPE=Release
+# sudo make install
+# git branch -D tagName-branch
+
+# UNINSTALL NEOVIM:
+# ===================
+# sudo rm /usr/local/bin/nvim
+# sudo rm -r /usr/local/share/nvim/
