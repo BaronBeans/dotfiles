ce8b472c0d01a03e0d72226490ca118244c03513
Sean Barnard <sean.barnard.94@hotmail.com>
2024-03-11 18:35:58 +0000

    copilot chat

--- nvim/lua/plugins/completion.lua
+++ nvim/lua/plugins/completion.lua
@@ -41,24 +41,24 @@ return {
 			mapping = cmp.mapping.preset.insert({
 				["<C-k>"] = cmp.mapping.select_prev_item(), -- previous suggestion
 				["<C-j>"] = cmp.mapping.select_next_item(), -- next suggestion
-				["<Tab>"] = cmp.mapping(function(fallback)
-					if cmp.visible() then
-						cmp.select_next_item()
-					elseif luasnip.expand_or_jumpable() then
-						luasnip.expand_or_jump()
-					else
-						fallback()
-					end
-				end, { "i", "s" }),
-				["<S-Tab>"] = cmp.mapping(function(fallback)
-					if cmp.visible() then
-						cmp.select_prev_item()
-					elseif luasnip.jumpable(-1) then
-						luasnip.jump(-1)
-					else
-						fallback()
-					end
-				end, { "i", "s" }),
+				-- ["<Tab>"] = cmp.mapping(function(fallback)
+				-- 	if cmp.visible() then
+				-- 		cmp.select_next_item()
+				-- 	elseif luasnip.expand_or_jumpable() then
+				-- 		luasnip.expand_or_jump()
+				-- 	else
+				-- 		fallback()
+				-- 	end
+				-- end, { "i", "s" }),
+				-- ["<S-Tab>"] = cmp.mapping(function(fallback)
+				-- 	if cmp.visible() then
+				-- 		cmp.select_prev_item()
+				-- 	elseif luasnip.jumpable(-1) then
+				-- 		luasnip.jump(-1)
+				-- 	else
+				-- 		fallback()
+				-- 	end
+				-- end, { "i", "s" }),
 				["<C-u>"] = cmp.mapping.scroll_docs(4), -- scroll up preview
 				["<C-d>"] = cmp.mapping.scroll_docs(-4), -- scroll down preview
 				["<C-Space>"] = cmp.mapping.complete({}), -- show completion suggestions
@@ -115,5 +115,6 @@ return {
 		-- use js snippets for ts
 		require("luasnip").filetype_extend("typescript", { "javascript" })
 		require("luasnip").filetype_extend("typescriptreact", { "javascript" })
+		require("luasnip").filetype_extend("javascriptreact", { "javascript" })
 	end,
 }
--- nvim/lua/plugins/compter.lua
+++ nvim/lua/plugins/compter.lua
@@ -1,6 +1,137 @@
 return {
 	"RutaTang/compter.nvim",
 	config = function()
-		require("compter").setup({})
+		require("compter").setup({
+			templates = {
+				-- for numbers
+				{
+					pattern = [[-\?\d\+]],
+					priority = 0,
+					increase = function(content)
+						content = tonumber(content)
+						return content + 1, true
+					end,
+					decrease = function(content)
+						content = tonumber(content)
+						return content - 1, true
+					end,
+				},
+				-- for lowercase alphabet
+				{
+					pattern = [[\l]],
+					priority = 0,
+					increase = function(content)
+						local ansiCode = string.byte(content) + 1
+						if ansiCode > string.byte("z") then
+							ansiCode = string.byte("a")
+						end
+						local char = string.char(ansiCode)
+						return char, true
+					end,
+					decrease = function(content)
+						local ansiCode = string.byte(content) - 1
+						if ansiCode < string.byte("a") then
+							ansiCode = string.byte("z")
+						end
+						local char = string.char(ansiCode)
+						return char, true
+					end,
+				},
+				-- for uppercase alphabet
+				{
+					pattern = [[\u]],
+					priority = 0,
+					increase = function(content)
+						local ansiCode = string.byte(content) + 1
+						if ansiCode > string.byte("Z") then
+							ansiCode = string.byte("A")
+						end
+						local char = string.char(ansiCode)
+						return char, true
+					end,
+					decrease = function(content)
+						local ansiCode = string.byte(content) - 1
+						if ansiCode < string.byte("A") then
+							ansiCode = string.byte("Z")
+						end
+						local char = string.char(ansiCode)
+						return char, true
+					end,
+				},
+				-- for date format: dd/mm/YYYY
+				{
+					pattern = [[\d\{2}/\d\{2}/\d\{4}]],
+					priority = 100,
+					increase = function(content)
+						local ts = vim.fn.strptime("%d/%m/%Y", content)
+						if ts == 0 then
+							return content, false
+						else
+							ts = ts + 24 * 60 * 60
+							return vim.fn.strftime("%d/%m/%Y", ts), true
+						end
+					end,
+					decrease = function(content)
+						local ts = vim.fn.strptime("%d/%m/%Y", content)
+						if ts == 0 then
+							return content, false
+						else
+							ts = ts - 24 * 60 * 60
+							return vim.fn.strftime("%d/%m/%Y", ts), true
+						end
+					end,
+				},
+				-- for date format: YYYY-mm-dd
+				{
+					pattern = [[\d\{4}-\d\{2}-\d\{2}]],
+					priority = 100,
+					increase = function(content)
+						local ts = vim.fn.strptime("%Y-%m-%d", content)
+						if ts == 0 then
+							return content, false
+						else
+							ts = ts + 24 * 60 * 60
+							return vim.fn.strftime("%Y-%m-%d", ts), true
+						end
+					end,
+					decrease = function(content)
+						local ts = vim.fn.strptime("%Y-%m-%d", content)
+						if ts == 0 then
+							return content, false
+						else
+							ts = ts - 24 * 60 * 60
+							return vim.fn.strftime("%Y-%m-%d", ts), true
+						end
+					end,
+				},
+				-- for boolean
+				{
+					pattern = [[\<\(true\|false\|TRUE\|FALSE\|True\|False\)\>]],
+					priority = 100,
+					increase = function(content)
+						local switch = {
+							["true"] = "false",
+							["false"] = "true",
+							["True"] = "False",
+							["False"] = "True",
+							["TRUE"] = "FALSE",
+							["FALSE"] = "TRUE",
+						}
+						return switch[content], true
+					end,
+					decrease = function(content)
+						local switch = {
+							["true"] = "false",
+							["false"] = "true",
+							["True"] = "False",
+							["False"] = "True",
+							["TRUE"] = "FALSE",
+							["FALSE"] = "TRUE",
+						}
+						return switch[content], true
+					end,
+				},
+			},
+		})
 	end,
 }
